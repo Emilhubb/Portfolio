83fe09a434185f4ce14d6f55c3b6646eb2c44340
Emil <kazimli_187@mail.ru>
2026-07-07 01:25:18 +0400

    replacing duo card into the aside section with hide&show functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import DuolingoCard from "@/components/DuolingoCard";
 import AboutCard from "../components/AboutCard";
 
 export default function Home() {
@@ -6,7 +5,6 @@ export default function Home() {
     <div className=" w-full min-h-screen p-4 sm:p-6 text-white">
       <div className="flex flex-col justify-center md:justify-start md:flex-row items-center md:items-start  gap-8">
         <AboutCard />
-        <DuolingoCard/>
       </div>
     </div>
   );
--- components/LanguageList.tsx
+++ components/LanguageList.tsx
@@ -1,8 +1,12 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 import English from "../public/english.png";
 import German from "../public/german.png";
 import Italian from "../public/italian.png";
 import Russian from "../public/russian.png";
+import Duolingo from "../public/duolingo.avif";
+import DuolingoCard from "./DuolingoCard";
 
 const languages = [
   { name: "English", flag: English },
@@ -12,21 +16,48 @@ const languages = [
 ];
 
 const LanguageList = () => {
+  const [toggleDuolingoCard, setToggleDuolingoCard] = useState(false);
+
+  const handleToggleDuolingoCard = () => {
+    setToggleDuolingoCard(!toggleDuolingoCard);
+  }
   return (
-    <ul className="flex flex-col p-4 gap-6 w-full py-4">
-      {languages.map((lang) => (
-        <li key={lang.name} className="flex items-center gap-2">
+    <>
+      <ul className="flex flex-col p-4 gap-6 w-full py-4">
+        {languages.map((lang) => (
+          <li key={lang.name} className="flex items-center gap-2">
+            <Image
+              src={lang.flag}
+              alt={`${lang.name} flag`}
+              className="w-5 h-5 rounded-full object-cover shrink-0"
+            />
+            <span className="text-white text-xl select-none ">{lang.name}</span>
+          </li>
+        ))}
+      </ul>
+      <ul className="px-4">
+        <li className="flex items-center gap-2">
           <Image
-            src={lang.flag}
-            alt={`${lang.name} flag`}
+            src={Duolingo}
+            alt="Duolingo logo"
             className="w-5 h-5 rounded-full object-cover shrink-0"
           />
-          <span className="text-white text-xl select-none ">
-            {lang.name}
+          <span
+            className="text-white/70 select-none italic"
+            onClick={() => setToggleDuolingoCard(!toggleDuolingoCard)}
+          >
+            {
+              toggleDuolingoCard ? "Hide Duolingo Card" : "Show Duolingo Card"  
+            }
           </span>
         </li>
-      ))}
-    </ul>
+      </ul>
+      {toggleDuolingoCard && (
+        <div className="mt-4 px-4 mb-8">
+          <DuolingoCard />
+        </div>
+      )}
+    </>
   );
 };
 
